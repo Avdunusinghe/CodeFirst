ae04fbb43810c419134cd83435e22a6765a60198
Ashen Dunusinghe <60811307+Avdunusinghe@users.noreply.github.com>
2021-07-16 08:28:14 +0530

    migrations

--- CodeFirst/DatabaseData/CodeFirstContext.cs
+++ CodeFirst/DatabaseData/CodeFirstContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CodeFirst.DatabaseData.Configurations;
+using CodeFirst.Model;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,7 +28,12 @@ namespace CodeFirst.DatabaseData
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.ApplyConfiguration(new AuthorConfiguration());
+            modelBuilder.ApplyConfiguration(new BookConfiguration());
         }
+
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
     }
 }
